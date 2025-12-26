8cb705b3de4b793c739b57c3d642ff3d4b1b9d00
Starlightnwn <furryidk114@gmail.com>
2025-12-27 03:21:18 +0800

    add:底部导航栏

---
+++ lib/pages/routes_pages.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'picture_change.dart';
+
+class RoutesPages extends StatefulWidget {
+  const RoutesPages({super.key});
+
+  @override
+  State<RoutesPages> createState() => _RoutesPagesState();
+}
+
+class _RoutesPagesState extends State<RoutesPages> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [const Homepage(), const PictureChange()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+      ),
+    );
+  }
+}
